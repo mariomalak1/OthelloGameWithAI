f5a87c94b02b10cb23dbe286dfe0e97eec3b7f32
mario malak <93185168+mariomalak1@users.noreply.github.com>
2024-05-10 03:50:04 +0300

    done start function

--- Models/GameManager.py
+++ Models/GameManager.py
@@ -1,18 +1,48 @@
 from . import Player, Board
 
 class GameManager:
+    _PlayWithComputer = 1
+    _PlayWithSomeOne = 2
     def __init__(self):
         self.board = None
         self.player1 = None
         self.player2 = None
+        self.playWay = 0
 
     # has all logic for the game play
     def game(self):
         pass
 
-    # will get all needed data from user -> like name, color , need to play with computer or someone as an opponent
+
+    # will get all needed data from user -> like name, color, need to play with computer or someone as an opponent
     def start(self):
-        pass
+        print("Welcome Othello Game")
+
+        # first player will be black
+        name = input("What's your name : ")
+        self.player1.name = name
+        self.player1.color = "black"
+
+        # ask him to play with computer or will play with someone
+        while True:
+            wayToPlay = input(f"hey {self.player1.name} if you want to play with computer press Y, else if you want to play with someone press X :")
+            if wayToPlay.lower() == "y":
+                self.playWay = GameManager._PlayWithComputer
+                self.player2.name = "computer"
+                self.player2.color = "white"
+                break
+            elif wayToPlay.lower() == "x":
+                self.playWay = GameManager._PlayWithSomeOne
+                name = input("What's his name : ")
+                self.player2.name = name
+                self.player2.color = "white"
+                break
+            else:
+                print("please enter valid input.")
+
+        # start the game pay
+        self.game()
+
 
     # will check that any player is winner or it's draw or nothing
     def checkWinner(self):
@@ -24,3 +54,4 @@ class GameManager:
 
     def getScore(self):
         pass
+
